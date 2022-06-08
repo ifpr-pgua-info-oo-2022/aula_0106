3c556142c203bc94a18905a81881fee733a71e4f
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2022-06-08 09:22:27 -0300

    parte1

--- MegaSena.java
+++ MegaSena.java
@@ -6,17 +6,25 @@ public class MegaSena{
         
         Scanner scan = new Scanner(System.in);
         int escolha;
+        int[] escolhas;
+
+        escolhas = new int[6];
         //int i;
 
         for(int i=0;i<6;i++){
-            System.out.println("Digite um número entre 1 e 60:");
-            escolha = scan.nextInt();
-            if(escolha < 1 || escolha > 60){
-                System.out.println("Valor inválido!!");
-            }else{
-                System.out.println("Digitou "+escolha);
-            }
-            
+            do{
+                System.out.println("Digite um número entre 1 e 60:");
+                escolha = scan.nextInt();
+                if(escolha < 1 || escolha > 60){
+                    System.out.println("Valor inválido!!");
+                }
+            }while(escolha<1 || escolha > 60);
+            System.out.println("Digitou "+escolha);
+            escolhas[i] = escolha;
+        }
+
+        for(int i=0;i<6;i++){
+            System.out.print(escolhas[i]+", ");
         }
         
 
