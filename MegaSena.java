41ec5edc1e7f312bd44cb26599f835baaabc7d93
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2022-06-08 10:43:07 -0300

    código finalizado

--- MegaSena.java
+++ MegaSena.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class MegaSena{
 
@@ -12,15 +13,13 @@ public class MegaSena{
         return false;
     }
 
-
-    public static void main(String[] args){
-        
+    public static int[] escolhasUsuario(){
         Scanner scan = new Scanner(System.in);
         int escolha;
         int[] escolhas;
         boolean flag;
         escolhas = new int[6];
-        //int i;
+        
 
         for(int i=0;i<6;i++){
             do{
@@ -41,11 +40,74 @@ public class MegaSena{
             System.out.println("Digitou "+escolha);
             escolhas[i] = escolha;
         }
+        return escolhas;
+    }
 
+    public static int[] sorteioComputador(){
+        int[] sorteados = new int[6];
+        int sorteado;
+        Random rnd = new Random();
+        boolean flag;
+
+        for(int i=0;i<6;i++){
+            do{
+                sorteado = rnd.nextInt(60)+1;
+                flag = verificaRepetido(sorteado, sorteados);
+            }while(flag == true);
+            sorteados[i]  = sorteado;
+        }
+
+        return sorteados;
+
+    }
+
+    public static int contaAcertos(int[] escolhas, int[] sorteados){
+
+        int contador=0;
+
+        for(int i=0;i<6;i++){
+            for(int j=0;j<6;j++){
+                if(escolhas[i] == sorteados[j]){
+                    contador = contador + 1;
+                }
+            }
+        }
+
+        return contador;
+
+    }
+
+
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+
+        int[] escolhas = escolhasUsuario();
+        int[] sorteados = sorteioComputador();
+
+        System.out.println("Escolhas do usuario:");
         for(int i=0;i<6;i++){
             System.out.print(escolhas[i]+", ");
         }
-        
+
+        System.out.println("Sorteio do computador:");
+        for(int i=0;i<6;i++){
+            System.out.print(sorteados[i]+", ");
+        }
+
+        int acertos = contaAcertos(escolhas, sorteados);
+
+        System.out.println("Você acertou "+acertos+" números");
+
+        if(acertos == 4){
+            System.out.println("Você fez a quadra!!");
+        }else if(acertos == 5){
+            System.out.println("Você fez a quina!!");
+        }else if(acertos == 6){
+            System.out.println("Você ficou ricoooo!!");
+        }else{
+            System.out.println("Perdeu uns pila!");
+        }
 
     }
 }
